0b158209a2574984267045eb5d15fc013a17460b
Generosa Litton <glitton@newrelic.com>
2018-11-28 13:43:23 -0800

    js works now

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//look at this https://codepen.io/april_lutheran/pen/JmpgGw
-
 let doorImage1 = document.getElementById("door1");
 let doorImage2 = document.getElementById("door2");
 let doorImage3 = document.getElementById("door3");
